5539114330880649bc433d67dda05cb300da5464
Santiago Lasso <santiago97.sl@gmail.com>
2024-08-31 17:36:12 -0500

    Add files via upload

---
+++ First_Project.py
@@ -0,0 +1,28 @@
+# Importación de librerías
+import random
+
+# Lista de palabras a adivinar
+palabras = []
+
+# Función para seleccionar una palabra aleatoria de la lista
+def seleccionar_palabra():
+    pass
+    
+# Función para mostrar la palabra oculta con letras adivinadas
+def mostrar_palabra_oculta(palabra, letras_adivinadas):
+    pass   
+
+# Función para dibujar el ahorcado según los intentos incorrectos
+def dibujar_ahorcado(intentos_incorrectos):
+    pass
+    
+"""-------------------------------------
+INICIO DE JUEGO
+-------------------------------------"""
+# Declaración de variables
+
+# Mensaje de bienvenida
+print("¡Bienvenido al Juego del Ahorcado!")
+
+while True:
+    pass
