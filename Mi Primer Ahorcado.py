b3e6d6eb8c4e2b91113ede6664320a19bda5520d
Santiago Lasso <santiago97.sl@gmail.com>
2024-08-31 22:10:15 -0500

    Update Mi Primer Ahorcado.py

--- Mi Primer Ahorcado.py
+++ Mi Primer Ahorcado.py
@@ -1,29 +1,24 @@
-# Importación de librerías
 import random
 
 # Lista de palabras a adivinar
-palabras = ["coding", "ganar"]
+palabras = ["tony", "programacion", "desarrollo", "aprendizaje", "proyecto"]
 
-# Función para seleccionar una palabra aleatoria de la lista
 def seleccionar_palabra():
-    word = random.choice(palabras)
-    return word
-    
-# Función para mostrar la palabra oculta con letras adivinadas
-def mostrar_palabra_oculta(palabra, letras_adivinadas):
-    palabra_mostrada = ""
+    # Función para seleccionar una palabra aleatoria de la lista
+    return random.choice(palabras)
 
+def mostrar_palabra_oculta(palabra, letras_adivinadas):
+    # Función para mostrar la palabra oculta con letras adivinadas
+    palabra_mostrada = ""
     for letra in palabra:
         if letra in letras_adivinadas:
             palabra_mostrada += letra
         else:
-            palabra_mostrada += "_"  
-
+            palabra_mostrada += "_"
     return palabra_mostrada
 
-# Función para dibujar el ahorcado según los intentos incorrectos
 def dibujar_ahorcado(intentos_incorrectos):
-
+    # Función para dibujar el ahorcado según los intentos incorrectos
     ahorcado = [
         "  ____ ",
         " |    |",
@@ -32,42 +27,35 @@ def dibujar_ahorcado(intentos_incorrectos):
         " |   / \\",
         "_|_"
     ]
-
     for i in range(intentos_incorrectos):
         print(ahorcado[i])
-    
+
 """-------------------------------------
 INICIO DE JUEGO
 -------------------------------------"""
-# Declaración de variables
 palabra = seleccionar_palabra()
 letras_adivinadas = []
 intentos_incorrectos = 0
 max_intentos = 6
 
-# Mensaje de bienvenida
 print("¡Bienvenido al Juego del Ahorcado!")
 
 while True:
     palabra_mostrada = mostrar_palabra_oculta(palabra, letras_adivinadas)
     print("\nPalabra: " + palabra_mostrada)
-    
-    letra = input("Ingresa una letra: ")
+    letra = input("Ingresa una letra: ").lower()
 
     if letra in letras_adivinadas:
-        print("ALERTA! Ya has ingresado esa letra.")
-        
+        print("Ya has adivinado esa letra.")
     elif letra in palabra:
         letras_adivinadas.append(letra)
-        
         if palabra_mostrada == palabra:
-            print("\nFelicidades! Has adivinado la palabra "+palabra)
+            print("\n¡Felicidades! Has adivinado la palabra: " + palabra)
             break
-
-    else: 
+    else:
         intentos_incorrectos += 1
         dibujar_ahorcado(intentos_incorrectos)
         if intentos_incorrectos == max_intentos:
-            print("\GAME OVER! La palabra era:"+palabra)
+            print("\n¡Has perdido! La palabra era: " + palabra)
             break
     
